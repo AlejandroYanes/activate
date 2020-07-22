ac139bc9cd63e0ebe34abc5985e7d6d7ca4e824f
Alejandro Yanes <alejandro.yanes94@gmail.com>
2020-07-22 09:15:15 -0400

    feat: added some more background images to the event card (#25)

---
+++ src/components/experience/EventCard/images.ts
@@ -0,0 +1,13 @@
+import backgroundImage1 from 'assets/images/cuba-1082913_1920.jpg';
+import backgroundImage2 from 'assets/images/IMG_20200102_174813.jpg';
+import backgroundImage3 from 'assets/images/IMG_20200105_202639.jpg';
+import backgroundImage4 from 'assets/images/IMG_20200229_115423.jpg';
+import backgroundImage5 from 'assets/images/IMG_20200229_133255.jpg';
+
+export const images = [
+  backgroundImage1,
+  backgroundImage2,
+  backgroundImage3,
+  backgroundImage4,
+  backgroundImage5,
+];
