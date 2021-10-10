9250bff417748073830b11d4ea3f0da50b67306f
ayanes <alejandro.yanes94@gmail.com>
2021-10-10 18:04:40 -0400

    [Alejandro] #138: removed all use of faker from the components - changed font type to .woff

---
+++ src/components/experience/Messages/messages.ts
@@ -0,0 +1,133 @@
+export type MessageModel = {
+	id: string;
+	text: string;
+	sent: boolean;
+};
+
+export const messages: MessageModel[] = [
+  {
+    id: '761A07E5-A2E6-8726-FE38-CA2444368912',
+    text: 'orci. Donec nibh. Quisque nonummy ipsum non',
+    sent: false
+  },
+  {
+    id: '4E017F44-DFDB-C3BD-9A3C-61CC3BEEEE2E',
+    text: 'metus. In nec orci. Donec nibh. Quisque nonummy ipsum',
+    sent: false
+  },
+  {
+    id: 'C6DDA9AE-7435-5CAA-8536-A9765DDA9A48',
+    text: 'sagittis placerat. Cras dictum ultricies ligula. Nullam',
+    sent: false
+  },
+  {
+    id: '529948C1-DEF0-1740-3268-8A48C4C03DA5',
+    text: 'mauris id sapien. Cras dolor dolor, tempus',
+    sent: true
+  },
+  {
+    id: '06481BDB-4478-ECA9-DEE1-91635FE6E3E3',
+    text: 'Suspendisse sed dolor. Fusce mi lorem, vehicula et,',
+    sent: false
+  },
+  {
+    id: '4BFC1B5F-DAB9-7007-EE40-2A9D3C48B822',
+    text: 'ridiculus mus. Aenean eget magna. Suspendisse tristique',
+    sent: true
+  },
+  {
+    id: 'B5C86934-4D68-2145-1DA5-CD91AC550A56',
+    text: 'arcu vel quam dignissim pharetra.',
+    sent: false
+  },
+  {
+    id: 'E4864981-D455-C35E-CE09-C3763638F638',
+    text: 'sit amet ornare lectus',
+    sent: false
+  },
+  {
+    id: 'BE9F4A51-741A-7E68-25CF-054890C91CA1',
+    text: 'eget metus. In nec orci. Donec',
+    sent: true
+  },
+  {
+    id: '5DD9B9B6-B1EB-684D-44CD-5B2754706649',
+    text: 'lacus vestibulum lorem, sit amet ultricies sem',
+    sent: false
+  },
+  {
+    id: '0A49560A-1C72-D37A-DD98-77A878DC684C',
+    text: 'a, malesuada id, erat. Etiam vestibulum massa rutrum magna.',
+    sent: false
+  },
+  {
+    id: '80874C29-3949-337A-71E2-787609E4CE65',
+    text: 'mi lacinia mattis. Integer eu',
+    sent: false
+  },
+  {
+    id: '4444821C-E825-26CD-1531-94B625723141',
+    text: 'sed sem egestas blandit.',
+    sent: true
+  },
+  {
+    id: 'C43CA100-E31F-B8A8-5FEF-11BB2D7697E9',
+    text: 'Duis ac arcu. Nunc mauris. Morbi non sapien',
+    sent: true
+  },
+  {
+    id: '46501739-5A94-329B-B634-9C9E9A16C489',
+    text: 'netus et malesuada fames ac turpis',
+    sent: false
+  },
+  {
+    id: '44B3F0C7-9469-4ACA-DC47-251E9219C4DD',
+    text: 'leo. Vivamus nibh dolor, nonummy',
+    sent: true
+  },
+  {
+    id: '8D09ED15-71A2-024E-ED74-9A92E6CAC119',
+    text: 'odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam',
+    sent: true
+  },
+  {
+    id: '4E99A551-EC80-4556-185A-ACC3E29D772A',
+    text: 'pellentesque a, facilisis non, bibendum sed, est. Nunc',
+    sent: true
+  },
+  {
+    id: '98A3CA9D-A43A-684B-8E4C-58160A8B1CF7',
+    text: 'Curabitur ut odio vel est tempor',
+    sent: true
+  },
+  {
+    id: 'C799D83B-BCB6-3989-79BE-E1F63A19AB44',
+    text: 'cursus luctus, ipsum leo elementum sem,',
+    sent: true
+  },
+  {
+    id: '8504DD92-CE8A-654C-159D-53768C8D3C6E',
+    text: 'pellentesque eget, dictum placerat, augue. Sed molestie. Sed',
+    sent: false
+  },
+  {
+    id: '6C8A3D1B-B2B4-486B-960C-B2DB7C0A14A8',
+    text: 'arcu vel quam dignissim pharetra. Nam',
+    sent: false
+  },
+  {
+    id: 'D83D357E-931B-86DD-7D9C-C0C33A9A8545',
+    text: 'erat. Etiam vestibulum massa rutrum magna. Cras',
+    sent: true
+  },
+  {
+    id: '46251737-838B-BDDC-EC07-A796D1EB1243',
+    text: 'dui. Cras pellentesque. Sed dictum. Proin eget',
+    sent: true
+  },
+  {
+    id: 'F3CB1BE9-7145-B87E-C650-A479A9BA68FC',
+    text: 'Nunc lectus pede, ultrices',
+    sent: true
+  }
+];
--- src/components/pages/EventDetails/Description.tsx
+++ src/components/pages/EventDetails/Description.tsx
@@ -1,18 +1,18 @@
 import React, { FunctionComponent } from 'react';
-import faker from 'faker';
 import { EventModel } from 'models/event';
 import { formatDateTime } from 'helpers';
 import { Paragraph, Text } from 'components/base-components/Typography';
 import FlexBox from 'components/base-components/FlexBox';
 import Attendance from 'components/experience/Attendance';
+import { paragraphs } from './description';
 
 interface Props {
   event: EventModel;
 }
 
-const description = new Array(6).fill(1).map((_, index) => (
+const description = paragraphs.map((text, index) => (
   <Paragraph padding="8px 8px 16px" size="large" key={index}>
-    {faker.lorem.lines(10)}
+    {text}
   </Paragraph>
 ));
 
